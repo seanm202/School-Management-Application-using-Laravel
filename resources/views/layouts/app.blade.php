97d6b001087a10a5d349cec93e6d4ce8e3588ed5
seanm202 <61375866+seanm202@users.noreply.github.com>
2026-03-14 12:43:10 +0530

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,12 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+       
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
-            @if ( Auth::user()->role ==   1)
+            @if ( Auth::user()->role == 1)
             @include('layouts.guest')
             @endif
 
