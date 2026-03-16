957823881d43492c566243ac32d6988b89243016
seanm202 <61375866+seanm202@users.noreply.github.com>
2026-03-16 22:50:03 +0530

    Update dashboard.blade.php

--- resources/views/Student/dashboard.blade.php
+++ resources/views/Student/dashboard.blade.php
@@ -1,21 +1,8 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-      <script src = "https://code.jquery.com/jquery-3.5.1.slim.min.js"
-      integrity = "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-      crossorigin = "anonymous">
-  </script>
-  <script src =
-"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-      integrity =
-"sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-      crossorigin = "anonymous">
-  </script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+ <meta name="csrf-token" content="{{ csrf_token() }}">
   <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -43,7 +30,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You'red logged in") }} {{ Auth::user()->name }}!
+                    {{ __("You're logged in") }} {{ Auth::user()->name }}!
                 </div>
             </div>
         </div>
@@ -118,7 +105,7 @@
         </div>
         @endif
                       @if(($att = \App\Models\StudentSubjectAttendance::where('student_subject_attendances.studentId','=',(\App\Models\Student::where('userId','=',Auth()->user()->userId)->select('studentId')->first()))
-                                                                      ->where('student_subject_attendances.hourId','=',(\App\Models\Hours::where('hours.status','=',1)->first())->hourIdr)
+                                                                      ->where('student_subject_attendances.hourId','=',(\App\Models\Hours::where('hours.status','=',1)->first())->hourId)
                                                                       ->select('id')->first())!=NULL)
 
                         @foreach(($att = \App\Models\StudentSubjectAttendance::where('student_subject_attendances.studentId','=',(\App\Models\Student::where('userId','=',Auth()->user()->userId)->select('studentId')->first()))
@@ -127,7 +114,7 @@
 
                       <form action="{{route('markTodaysAttendanceStudent')}}" method="POST" name="markTodaysAttendanceStudent" id="markTodaysAttendanceStudent">
                       {{ csrf_field() }}{{ method_field('POST') }}
-                        {{Form::label('inOrOut', 'Present')}}{{Form::radio('inOrOut', 1,array('class'=>'form-control'))}}
+                        {{Form::label('inOrOut', 'Present')}}{{ Form::radio('inOrOut', 1, false, ['class'=>'form-control']) }}
                         <br>
                         {{Form::hidden('userRole',4)}}
                         {{Form::label('inOrOut', 'Absent')}}{{Form::radio('inOrOut', 0,'checked',array('class'=>'form-control'))}}
@@ -148,7 +135,7 @@
                       {{ csrf_field() }}{{ method_field('POST') }}
                         {{Form::label('inOrOut', 'Present')}}{{Form::radio('inOrOut', 1)}}
                         <br>
-                        {{Form::hidden('userRole',4)}}}}
+                        {{Form::hidden('userRole',4)}}
                         <input type="hidden" name="attendanceDataId" value="{{$atst->id}}"></input>
                         {{Form::label('inOrOut', 'Absent')}}{{Form::radio('inOrOut', 0,'checked',array('class'=>'form-control'))}}
                         <br>
