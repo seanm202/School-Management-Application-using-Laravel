f7d469bc3945cb2371cc7c0f34e16e388fb76b95
seanm202 <61375866+seanm202@users.noreply.github.com>
2026-03-16 23:15:42 +0530

    Update attendance.blade.php

--- resources/views/Student/attendance.blade.php
+++ resources/views/Student/attendance.blade.php
@@ -1,21 +1,6 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
-      <script src = "https://code.jquery.com/jquery-3.5.1.slim.min.js"
-      integrity = "sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
-      crossorigin = "anonymous">
-  </script>
-  <script src =
-"https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-      integrity =
-"sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
-      crossorigin = "anonymous">
-  </script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
   <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -53,7 +38,7 @@
         </div>
         @endif
 
-                  @if(count($studentAttendanceStatus = \App\Models\studentSubjectAttendance::where('student_subject_attendances.studentId','=',(\App\Models\student::where('students.userId','=',Auth::user()->userId)->where('students.batchId','=',$currentBatchId)->select('students.studentId')->first())->studentId)->where('student_subject_attendances.batchId','=',$currentBatchId)
+                  @if(count($studentAttendanceStatus = \App\Models\StudentSubjectAttendance::where('student_subject_attendances.studentId','=',(\App\Models\Student::where('students.userId','=',Auth::user()->userId)->where('students.batchId','=',1)->select('students.studentId')->first())->studentId)->where('student_subject_attendances.batchId','=',1)
                            ->join('subjects','subjects.subjectId','=','student_subject_attendances.subjectId')
                            ->join('days','days.dayId','=','student_subject_attendances.dayId')
                            ->join('hours','hours.hourId','=','student_subject_attendances.hourId')
@@ -77,7 +62,7 @@
                       </tr>
                       </thead>
                       <tbody>
-                      @foreach(($studentAttendanceStatus = \App\Models\studentSubjectAttendance::where('student_subject_attendances.studentId','=',(\App\Models\student::where('students.userId','=',Auth::user()->userId)->where('students.batchId','=',$currentBatchId)->select('students.studentId')->first())->studentId)->where('student_subject_attendances.batchId','=',$currentBatchId)
+                      @foreach(($studentAttendanceStatus = \App\Models\StudentSubjectAttendance::where('student_subject_attendances.studentId','=',(\App\Models\Student::where('students.userId','=',Auth::user()->userId)->where('students.batchId','=',1)->select('students.studentId')->first())->studentId)->where('student_subject_attendances.batchId','=',1)
                                ->join('daily_teacher_allocation','daily_teacher_allocation.daily_Teacher_AllocationId','=','student_subject_attendances.dailyTeacherAllocationId')
                                ->join('students','students.studentId','=','student_subject_attendances.studentId')
                                ->join('subjects','subjects.subjectId','=','student_subject_attendances.subjectId')
