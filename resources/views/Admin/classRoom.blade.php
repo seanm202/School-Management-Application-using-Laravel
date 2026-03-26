a80659e7b3df1c8b2f2a4959a67dab444d1158e3
seanm202 <61375866+seanm202@users.noreply.github.com>
2026-03-26 20:01:42 +0530

    Update classRoom.blade.php

--- resources/views/Admin/classRoom.blade.php
+++ resources/views/Admin/classRoom.blade.php
@@ -433,7 +433,7 @@ function getClassRooms(){
     </script>
 
 
-    <div class="py-12" id="viewEditClassrooms">
+    <!-- <div class="py-12" id="viewEditClassrooms">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
@@ -551,7 +551,7 @@ function getClassRooms(){
                                                           </div>
                                                           </div>
                                                           </div>
-                                                          </div>
+                                                          </div> -->
 
 
 
