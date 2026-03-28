89ae65378201cd4767d0ad2ec3e592d504089e69
seanm202 <61375866+seanm202@users.noreply.github.com>
2026-03-28 19:30:06 +0530

    Update admin.js

--- public/js/Admin/admin.js
+++ public/js/Admin/admin.js
@@ -433,6 +433,36 @@ showSuccess();
 
                                       });
 
+// Delete Hour
+$(function () {
+
+        $.ajaxSetup({
+            headers: {
+                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+            }
+      });
+
+
+      $('#deleteHourDetails').click(function (e) {
+  e.preventDefault();
+  var deleteHourDetails = $('#deleteHour').attr('action');
+  $.ajax({
+  data: $('#deleteHour').serialize(),
+  url: deleteHourDetails,
+  type: "POST",
+  dataType: 'json',
+  success: function (data) {
+      getAllData();
+showDeleteSuccess();
+  },
+  error: function (xhr) {
+  console.log(xhr.responseText);
+  }
+                                  });
+                                  });
+
+                                    });
+
 
 $(function () {
 
