b01880e416ed68bf9836d264557180769f11d3d1
seanm202 <61375866+seanm202@users.noreply.github.com>
2026-07-28 18:58:45 +0530

    Updated app.php file
    Updated app.php file

--- config/app.php
+++ config/app.php
@@ -54,7 +54,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    // 'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://s-olia.xyz/'),
 
     'asset_url' => env('ASSET_URL'),
 
