a75f96c54b5a82ac7b84cec1b7ae710da629893e
Sean <seansanthoshmanjaly@gmail.com>
2026-07-28 13:07:41 +0530

    Sync check 4 at 13:07 on 28-07-2026

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>My Schools Online</title>
+    <title>My Schoolsd Online</title>
 
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
 
